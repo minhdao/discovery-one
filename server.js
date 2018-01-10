df5b08f87573c4e78f2923ba76b31d4d299cf47c
minhdao <minhdao6@gmail.com>
2018-01-10 21:06:56 +0700

    disable maintainance middleware

--- server.js
+++ server.js
@@ -38,9 +38,9 @@ app.use((req, res, next) => {
     });
     next();
 });
-app.use((req, res, next) => {
-    res.render('maintainance.hbs');
-});
+// app.use((req, res, next) => {
+//     res.render('maintainance.hbs');
+// });
 app.use(express.static(__dirname + '/public'));
 
 // route/hanlder configuration
