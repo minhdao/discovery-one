59bbd184708b194a700e1f56908f891b22e76527
minhdao <minhdao6@gmail.com>
2018-01-15 18:24:27 +0700

    call request package

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 const stellar = require('stellar-sdk');
+const request = require('request');
 
 // app variables
 var author = 'mdao';
